92cb7f5e21f3db7e223e911a64d8af417a3dee6e
asif-kamal <asif.kamal1489@gmail.com>
2024-10-16 12:14:08 -0400

    added @Table to distinguish from user keyword in postgreSQL

--- src/main/java/com/smallcodingproject/full_stack_backend/model/User.java
+++ src/main/java/com/smallcodingproject/full_stack_backend/model/User.java
@@ -3,10 +3,12 @@ package com.smallcodingproject.full_stack_backend.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "users") // added @Table to distinguish from user keyword in postgreSQL
 public class User {
     @Id
     @GeneratedValue
