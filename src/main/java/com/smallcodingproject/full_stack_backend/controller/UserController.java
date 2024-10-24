9a7196fc14cdea763d26fde8cc7f0cce89f44add
asif-kamal <asif.kamal1489@gmail.com>
2024-10-23 20:05:23 -0400

    created getUserById method

--- src/main/java/com/smallcodingproject/full_stack_backend/controller/UserController.java
+++ src/main/java/com/smallcodingproject/full_stack_backend/controller/UserController.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.smallcodingproject.full_stack_backend.exception.UserNotFoundException;
 import com.smallcodingproject.full_stack_backend.model.User;
 import com.smallcodingproject.full_stack_backend.repository.UserRepository;
 
@@ -30,5 +32,10 @@ public class UserController {
     @GetMapping()
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
     }
 }
